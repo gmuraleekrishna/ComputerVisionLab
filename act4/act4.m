af7382fda32dbf90d3aff00a0bd3f9409cb211fb
Muraleekrishna Gopinathan <muraleekrishnagc@gmail.com>
2018-03-08 21:13:05 +1100

    Add gaussian filter and fix image displays

--- act4/act4.m
+++ act4/act4.m
@@ -1,4 +1,4 @@
-img = imread('IMG_20180302_203415.jpg');
+img = imread('face_01_u6528602_resized.jpg');
 resized_img = imresize(img, [512 512]);
 noisy_img = resized_img;
 
@@ -18,11 +18,34 @@ for i = 1:size(rand_nos, 1)
     end
 end
 
+median_f_image = median_filter(noisy_img);
 
-figure
+sigma = 1.1;
+gaussian_kernel = fspecial('gaussian', [9 9], sigma);
+gaussian_f_image = gauss_filter(noisy_img, gaussian_kernel);
+
+figure(1)
+subplot(1,3,1);
 imshow(noisy_img);
-output_image = median_filter(noisy_img);
-figure
-imshow(output_image)
-figure
-imshow(sobel_filter(output_image));
+title('Noisy Image');
+
+subplot(1,3,2);
+imshow(median_f_image);
+title('Median filtered Image');
+
+subplot(1,3,3);
+imshow(gaussian_f_image);
+title('Gaussian filtered Image');
+
+figure(2)
+
+subplot(1,2,1);
+imshow(sobel_filter(resized_img));
+title('Sobel filtered Image');
+
+subplot(1,2,2);
+sobel_h = fspecial('sobel');
+imshow(imfilter(resized_img, sobel_h));
+title('Inbuild Sobel filtered Image');
+
+
---
+++ act4/gauss_filter.m
@@ -0,0 +1,14 @@
+function output_image = gauss_filter(noisy_image, gausskernel)
+    img = padarray(noisy_image, [4 4], 0, 'both');
+    stacked_kernel = cat(3, gausskernel, gausskernel, gausskernel);
+    [r, c, ~] = size(noisy_image);
+    output_image = zeros(size(noisy_image));
+    
+    for i = 1:r
+        for j = 1:c
+            slice = double(img(i:i+8, j:j+8, :));
+            output_image(i, j, :) = sum(sum(slice .* stacked_kernel));
+        end
+    end
+    output_image = uint8(output_image);
+end
