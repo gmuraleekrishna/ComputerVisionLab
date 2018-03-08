652681a8078715eba44781ed8d11066344870149
Muraleekrishna Gopinathan <muraleekrishnagc@gmail.com>
2018-03-08 16:42:00 +1100

    Fix: Kernal size in gaussian filter

--- act3/act3.m
+++ act3/act3.m
@@ -1,18 +1,27 @@
-img = imread('IMG_20180302_203415.jpg');
+close all;
+img = imread('face_01_u6528602.jpg');
+img = imcrop(img, [450, 100, 500, 400]);
 resized_img = imresize(img, [512 512]);
 noisy_img = imnoise(resized_img, 'gaussian', 0, 0.0138);
 hist_ri = imhist(resized_img);
 hist_ni = imhist(noisy_img);
-% figure
-% plot(hist_ri);
-% figure
-% plot(hist_ni);
-figure
-imshow(noisy_img);
+
 sigma = 1.1;
 gaussian_kernel = fspecial('gaussian', [9 9], sigma);
 output_image = gauss_filter(noisy_img, gaussian_kernel);
+
+
+
 figure
-imshow(output_image)
-figure
-imshow(imgaussfilt(noisy_img, sigma))
+subplot(1,3,1);
+imshow(noisy_img);
+title('Noisy Image');
+
+subplot(1,3,2);
+imshow(output_image);
+title('Filtered Image');
+
+
+subplot(1,3,3);
+imshow(imgaussfilt(noisy_img, sigma));
+title('Filtered Using imgaussfilt');
--- act3/gauss_filter.m
+++ act3/gauss_filter.m
@@ -6,8 +6,8 @@ function output_image = gauss_filter(noisy_image, gausskernel)
     
     for i = 1:r
         for j = 1:c
-            slice = double(img(i:i+2, j:j+2, :));
-            output_image(i, j, :) = (slice .* stacked_kernel);
+            slice = double(img(i:i+8, j:j+8, :));
+            output_image(i, j, :) = sum(sum(slice .* stacked_kernel));
         end
     end
     output_image = uint8(output_image);
