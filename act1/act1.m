43b969ea689674d1e872f068e4c095b867947103
Muraleekrishna Gopinathan <muraleekrishnagc@gmail.com>
2018-03-12 09:56:22 +1100

    Finish Task 1

--- act1/act1.m
+++ act1/act1.m
@@ -1,43 +1,68 @@
 % resize and hist eq images
 close all
-[orig_img, ~] = imread('face_01_u6528602_resized.jpg');
+
+
+[orig_img, ~] = imread('face_03_u6528602.jpg');
 orig_img = imresize(orig_img, [512, 768]);
 r = orig_img(:, :, 1); % get R channel
 g = orig_img(:, :, 2); % get G channel
 b = orig_img(:, :, 3); % get B channel
-
-hist_eq_img = histeq(orig_img); % get equalized histogram
-rh = hist_eq_img(:, :, 1);
-gh = hist_eq_img(:, :, 2);
-bh = hist_eq_img(:, :, 3);
 
 figure
 subplot(1,2,1);
 imshow(orig_img)
 title('Original Image');
 
-subplot(1,2,2);
-imshow(hist_eq_img)
-title('Histogram Equalised Image');
 
-figure
+subplot(1,2,2);
 subplot(2,3,1);
-imhist(r); % show histogram R
+imshow(r); % show histogram R
 title('R channel');
 subplot(2,3,2);
-imhist(g); % show histogram G
+imshow(g); % show histogram G
 title('G channel');
 subplot(2,3,3);
-imhist(b); % show histogram B
+imshow(b); % show histogram B
 title('B channel');
 
 subplot(2,3,4);
-imhist(rh);
-title('Equlised R channel');
+imhist(r); % show histogram R
+title('R channel histogram');
 subplot(2,3,5);
-imhist(gh);
-title('Equlised G channel');
+imhist(g); % show histogram G
+title('G channel histogram');
 subplot(2,3,6);
-imhist(bh);
-title('Equlised B channel');
-imwrite(hist_eq_img, 'face_01_u6528602_hist_eqd.jpg');
+imhist(b); % show histogram B
+title('B channel histogram');
+
+hist_eq_img = histeq(orig_img); % get equalized histogram
+rh = hist_eq_img(:, :, 1);
+gh = hist_eq_img(:, :, 2);
+bh = hist_eq_img(:, :, 3);
+
+
+figure
+subplot(1,2,1);
+imshow(hist_eq_img)
+title('Histogram Equalised Image');
+
+subplot(1,2,2);
+subplot(2,3,1);
+imshow(hist_eq_img(:,:,1)); % show histogram R
+title('R channel');
+subplot(2,3,2);
+imshow(hist_eq_img(:,:,1)); % show histogram G
+title('G channel');
+subplot(2,3,3);
+imshow(hist_eq_img(:,:,1)); % show histogram B
+title('B channel');
+
+subplot(2,3,4);
+imhist(rh); % show histogram R
+title('R channel histogram');
+subplot(2,3,5);
+imhist(gh); % show histogram G
+title('G channel histogram');
+subplot(2,3,6);
+imhist(bh); % show histogram B
+title('B channel histogram');
