245aae0a47f5068bb1400359a6e59cbe22c9568f
Muraleekrishna Gopinathan <muraleekrishnagc@gmail.com>
2018-03-12 10:04:13 +1100

    Scale up the rotated image to fix corner trimming

--- act6/act6.m
+++ act6/act6.m
@@ -1,30 +1,20 @@
 close all;
 img = imread('Lenna.png');
-[r, c, ~] = size(img);
-rotated_img = zeros(size(img));
-theta = 270;
 
-T = [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
-mid_x = r/2;
-mid_y = c/2;
+theta = 10;
 
-
-for x = 1:r
-    for y = 1:c
-        pos = T * [(x-mid_x) (y-mid_y)]';
-        x_ = round(pos(1)) + mid_x;
-        y_ = round(pos(2)) + mid_y;
-        if(x_ <= c && y_ <= r && x_ >= 1 && y_ >= 1)
-            rotated_img(x_, y_, :) = img(x, y, :);
-        end
-    end
-end
+rotated_img = my_rotate(img, theta);
+matlab_rotated_img = imrotate(img, theta);
 
 figure
-subplot(1,2,1);
-imshow(uint8(rotated_img));
+subplot(1,3,1);
+imshow(rotated_img);
 title('Rotated image');
 
-subplot(1,2,2);
-imshow(imrotate(img, theta));
+subplot(1,3,2);
+imshow(matlab_rotated_img);
 title('Rotated image using imrotate');
+
+subplot(1,3,3);
+imshow(rotated_img - matlab_rotated_img);
+title('Compared');
